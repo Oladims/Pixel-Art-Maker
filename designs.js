82c602be44986bd627e5220af651b64d5a21d409
Ibrahim Olufowora <ioolofowora@sidmach.com>
2018-07-01 23:32:37 +0100

    Added modal that contains Instructions on how to use the App

--- designs.js
+++ designs.js
@@ -12,8 +12,40 @@ function changeColor() {
   var textcolor = textcolors[Math.floor(Math.random() * textcolors.length)];
   $('body').animate({ backgroundColor: bgcolor }, 1500);
   $('.header').animate({ color: textcolor }, 500);
+  $('#myModal').animate({ color: textcolor }, 1500);
+
+
 }
 
+// MODAL SECTION
+// Get the modal
+var modal = $('#myModal');
+
+// Get the button that opens the modal
+var btn = $("#myBtn");
+
+// Get the <span> element that closes the modal
+var span = $(".close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+// End of Modal Section
+
+//BEGINNING OF GRID SECTION
 // When size and background is submitted by the user, call makeGrid() and tableBackground()
 $('#sizePicker').on('submit', function (e) {
   e.preventDefault();
