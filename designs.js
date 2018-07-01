366aa94c9a9f7e33dd1c2c8e86295c3715d279cc
Ibrahim Olufowora <ioolofowora@sidmach.com>
2018-07-01 23:46:48 +0100

    fixed bugs

--- designs.js
+++ designs.js
@@ -16,36 +16,31 @@ function changeColor() {
 
 
 }
-
-// MODAL SECTION
 // Get the modal
-var modal = $('#myModal');
+var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = $("#myBtn");
+var btn = document.getElementById('myBtn');
 
 // Get the <span> element that closes the modal
-var span = $(".close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
+btn.onclick=function() {
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
-// End of Modal Section
-
-//BEGINNING OF GRID SECTION
 // When size and background is submitted by the user, call makeGrid() and tableBackground()
 $('#sizePicker').on('submit', function (e) {
   e.preventDefault();
